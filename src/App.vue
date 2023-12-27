0c2c9bd7a087eafd1a839f58132f82c42a03a0dd
ar_parvez <arparvez50@gmail.com>
2023-12-27 21:59:32 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Navigation from './components/Navigation.vue';
+import Herosection from './components/Herosection.vue';
+import Iconbox from './components/Iconbox.vue'
+</script>
+
+<template>
+  <Navigation/>
+  <Herosection/>
+  <Iconbox/>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Herosection.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+<template>
+  <section class="container mx-auto my-4 flex sm:flex-col-reverse md:flex-row items-center">
+    <div class="py-3">
+      <p>Hi there,</p>
+      <h2 class="my-3 text-2xl font-bold">I'm John Doe</h2>
+      <h4>Front-end developer | WordPress Developer | Shopify Developer</h4>
+      <a class="inline-block mt-3" href="#">Contact Now 🗯</a>
+    </div>
+    <div class="py-3">
+      <img src="../assets/dev-profile.png" class=" max-w-[500px]">
+    </div>
+  </section>
+</template>
+<style>
+
+</style>
