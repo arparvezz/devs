4d32fc476b2340c93a0a57d420ab9cc827419953
ar_parvez <arparvez50@gmail.com>
2024-01-03 01:06:01 +0600

    About section added

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import Navigation from './components/Navigation.vue';
 import Herosection from './components/Herosection.vue';
 import Iconbox from './components/Iconbox.vue'
+import About from './components/About.vue'
 </script>
 
 <template>
   <Navigation/>
   <Herosection/>
   <Iconbox/>
+  <About/>
 </template>
 
 <style scoped>
---
+++ src/components/About.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+<template>
+  <div class="container mx-auto my-5 flex flex-col-reverse md:flex-row space-x-0 md:space-x-3 lg:space-x-5">
+    <div class="py-4 sm:w-full md:w-1/2 flex flex-col justify-center items-start">
+      <h2 class="mb-3 text-xl font-bold">Hello, I'm parvez</h2>
+      <p class="mb-2">Junior <span class="text-green-400">Vue</span> Developer || Shopify Developer || WordPress Expert</p>
+      <p>"I'm Ar Parvez. Web Design is one of my passions, as well as WordPress and Shopify development. I have experience with WordPress, Elementor, Shopify & custom web design. In 2018, I completed my first professional course in web design and development. In 2020 I was hired by Prep Bangladesh as a Web designer & WordPress expert. My previous experience includes working for Codemanbd and two other organizations as a web designer and support specialist. For more than 40 clients, I have customized, designed, redesigned, and developed more than 50+ websites. Quality work is my priority."</p>
+    </div>
+    <div class="sm:w-full md:w-1/2">
+      <img src="https://arparvez.com/wp-content/uploads/2023/05/Ar-parvez.jpg" alt="">
+    </div>
+  </div>
+</template>
+<style></style>
